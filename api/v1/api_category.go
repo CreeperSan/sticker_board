ad425df9135f0f263c3c3260b2fe463158b331b5
CreeperSan <admin@creepersan.com>
2021-08-30 08:50:41 +0800

    migrate api_module's v1 references to v2

--- api/v1/api_category.go
+++ api/v1/api_category.go
@@ -3,6 +3,8 @@ package ApiV1
 import (
 	"github.com/kataras/iris/v12"
 	ApiMiddleware "sticker_board/api/middleware"
+	Formatter "sticker_board/lib/formatter"
+	StickerModule "sticker_board/sticker/manager"
 )
 
 func InitializeCategory(app *iris.Application){
@@ -20,91 +22,87 @@ func InitializeCategory(app *iris.Application){
 func createCategory(ctx iris.Context){
 	type RequestParams struct {
 		CategoryName string `json:"category_name"`
-		Icon    string `json:"icon"`
-		Color   int `json:"color"`
+		Icon         string `json:"icon"`
+		Color        int    `json:"color"`
 	}
 	type ResponseParams struct {
 		Code    int    `json:"code"`
 		Message string `json:"msg"`
 	}
 
-	//authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
-	//
-	//// parse params
-	//requestParams := RequestParams{}
-	//err := ctx.ReadJSON(&requestParams)
-	//if err != nil {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 406,
-	//		Message: "Params Error",
-	//	})
-	//	return
-	//}
-	//
-	//// can't make category color to transparent
-	//if requestParams.Color <= 0 {
-	//	requestParams.Color = 0xFF000000
-	//}
-	//requestParams.Color = 0xFF000000 // Currently not support custom category color
-	//requestParams.Icon = "icon:category_default" // Currently not support custom category color
-	//
-	//if !Formatter.CheckStringWithLength(requestParams.CategoryName, 1, 30) {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 406,
-	//		Message: "Params Error",
-	//	})
-	//}
-	//
-	//// TODO
-	////StickerDatabase.CreateCategory(authResult.AccountID, 0, requestParams.CategoryName, requestParams.Icon, requestParams.Color, "")
-	//
-	//ctx.JSON(ResponseParams{
-	//	Code: 200,
-	//	Message: "Success",
-	//})
+	authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
+
+	// parse params
+	requestParams := RequestParams{}
+	err := ctx.ReadJSON(&requestParams)
+	if err != nil {
+		ctx.JSON(ResponseParams{
+			Code: 406,
+			Message: "Params Error",
+		})
+		return
+	}
+
+	// can't make category color to transparent
+	if requestParams.Color <= 0 {
+		requestParams.Color = 0xFF000000
+	}
+	requestParams.Color = 0xFF000000 // Currently not support custom category color
+	requestParams.Icon = "icon:category_default" // Currently not support custom category color
+
+	if !Formatter.CheckStringWithLength(requestParams.CategoryName, 1, 30) {
+		ctx.JSON(ResponseParams{
+			Code: 406,
+			Message: "Params Error",
+		})
+	}
+
+	createResult := StickerModule.GetOperator().CreateCategory(authResult.AccountID, "", requestParams.CategoryName, requestParams.Icon, requestParams.Color)
+
+	ctx.JSON(ResponseParams{
+		Code: createResult.Code,
+		Message: createResult.Message,
+	})
 }
 
 func deleteCategory(ctx iris.Context){
 	type RequestParams struct {
-		CategoryID uint `json:"category_id"`
+		CategoryID string `json:"category_id"`
 	}
 	type ResponseParams struct {
 		Code    int    `json:"code"`
 		Message string `json:"msg"`
 	}
 
-	//authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
-	//
-	//// parse params
-	//requestParams := RequestParams{}
-	//err := ctx.ReadJSON(&requestParams)
-	//if err != nil {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 406,
-	//		Message: "Params Error",
-	//	})
-	//	return
-	//}
-	//
-	//StickerDatabase.DeleteCategory(authResult.AccountID, requestParams.CategoryID)
-	//
-	//ctx.JSON(ResponseParams{
-	//	Code: 200,
-	//	Message: "Success",
-	//})
+	authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
+
+	// parse params
+	requestParams := RequestParams{}
+	err := ctx.ReadJSON(&requestParams)
+	if err != nil {
+		ctx.JSON(ResponseParams{
+			Code: 406,
+			Message: "Params Error",
+		})
+		return
+	}
+
+	deleteResult :=  StickerModule.GetOperator().DeleteCategory(authResult.AccountID, requestParams.CategoryID)
+
+	ctx.JSON(ResponseParams{
+		Code: deleteResult.Code,
+		Message: deleteResult.Message,
+	})
 }
 
 func queryCategoryList(ctx iris.Context){
 	type ResponseParamsItem struct {
-		CategoryID uint   `json:"category_id"`
-		ParentID   uint   `json:"parent_id"`
-		CreateTime int    `json:"create_time"`
-		UpdateTime int    `json:"update_time"`
+		CategoryID string   `json:"category_id"`
+		CreateTime int64    `json:"create_time"`
+		UpdateTime int64    `json:"update_time"`
 		Name       string `json:"name"`
 		Icon       string `json:"icon"`
 		Color      int    `json:"color"`
-		Sort       int    `json:"sort"`
-		Extra      string `json:"extra"`
 	}
 	type ResponseParams struct {
 		Code    int                  `json:"code"`
@@ -112,36 +110,33 @@ func queryCategoryList(ctx iris.Context){
 		Data    []ResponseParamsItem `json:"data"`
 	}
 
-	//authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
-	//
-	//queryResponse := StickerDatabase.QueryAllCategory(authResult.AccountID)
-	//
-	//if queryResponse.Code != 200 {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 500,
-	//		Message: queryResponse.Message,
-	//	})
-	//}
-	//
-	//var dataList []ResponseParamsItem
-	//for _, tmpItem := range queryResponse.Data {
-	//	dataList = append(dataList, ResponseParamsItem{
-	//		CategoryID: tmpItem.ID,
-	//		CreateTime: tmpItem.CreateTime,
-	//		UpdateTime: tmpItem.UpdateTime,
-	//		Name: tmpItem.Name,
-	//		Icon: tmpItem.Icon,
-	//		Color: tmpItem.Color,
-	//		Sort: tmpItem.Sort,
-	//		Extra: tmpItem.Extra,
-	//		ParentID: tmpItem.ParentID,
-	//	})
-	//}
-	//
-	//ctx.JSON(ResponseParams{
-	//	Code: 200,
-	//	Message: "Success",
-	//	Data: dataList,
-	//})
+	authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
+	categoriesListResult := StickerModule.GetOperator().FindAllCategory(authResult.AccountID)
+
+	if !categoriesListResult.IsSuccess() {
+		ctx.JSON(ResponseParams{
+			Code: categoriesListResult.Code,
+			Message: categoriesListResult.Message,
+		})
+		return
+	}
+
+	var dataList []ResponseParamsItem
+	for _, tmpItem := range categoriesListResult.Categories {
+		dataList = append(dataList, ResponseParamsItem{
+			CategoryID: tmpItem.ID.Hex(),
+			CreateTime: tmpItem.CreateTime,
+			UpdateTime: tmpItem.UpdateTime,
+			Name: tmpItem.Name,
+			Icon: tmpItem.Icon,
+			Color: tmpItem.Color,
+		})
+	}
+
+	ctx.JSON(ResponseParams{
+		Code: 200,
+		Message: "Success",
+		Data: dataList,
+	})
 }
 
--- api/v1/api_tag.go
+++ api/v1/api_tag.go
@@ -3,6 +3,8 @@ package ApiV1
 import (
 	"github.com/kataras/iris/v12"
 	ApiMiddleware "sticker_board/api/middleware"
+	Formatter "sticker_board/lib/formatter"
+	StickerModule "sticker_board/sticker/manager"
 )
 
 func InitializeTag(app *iris.Application){
@@ -27,81 +29,80 @@ func createTag(ctx iris.Context){
 		Message string `json:"msg"`
 	}
 
-	//authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
-	//
-	//// parse params
-	//requestParams := RequestParams{}
-	//err := ctx.ReadJSON(&requestParams)
-	//if err != nil {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 406,
-	//		Message: "Params Error",
-	//	})
-	//	return
-	//}
-	//
-	//// can't make tag color to transparent
-	//if requestParams.Color <= 0 {
-	//	requestParams.Color = 0xFF000000
-	//}
-	//requestParams.Color = 0xFF000000 // Currently not support custom tag color
-	//requestParams.Icon = "icon:tag_default" // Currently not support custom tag color
-	//
-	//if !Formatter.CheckStringWithLength(requestParams.TagName, 1, 30) {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 406,
-	//		Message: "Params Error",
-	//	})
-	//}
-	//
-	//StickerDatabase.CreateTag(authResult.AccountID, requestParams.TagName, requestParams.Icon, requestParams.Color, "")
-	//
-	//ctx.JSON(ResponseParams{
-	//	Code: 200,
-	//	Message: "Success",
-	//})
+	authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
+
+	// parse params
+	requestParams := RequestParams{}
+	err := ctx.ReadJSON(&requestParams)
+	if err != nil {
+		ctx.JSON(ResponseParams{
+			Code: 406,
+			Message: "Params Error",
+		})
+		return
+	}
+
+	// can't make tag color to transparent
+	if requestParams.Color <= 0 {
+		requestParams.Color = 0xFF000000
+	}
+	requestParams.Color = 0xFF000000 // Currently not support custom tag color
+	requestParams.Icon = "icon:tag_default" // Currently not support custom tag color
+
+	if !Formatter.CheckStringWithLength(requestParams.TagName, 1, 30) {
+		ctx.JSON(ResponseParams{
+			Code: 406,
+			Message: "Params Error",
+		})
+	}
+
+	createResult := StickerModule.GetOperator().CreateTag(authResult.AccountID, requestParams.TagName, requestParams.Icon, requestParams.Color)
+
+	ctx.JSON(ResponseParams{
+		Code: createResult.Code,
+		Message: createResult.Message,
+	})
 }
 
 func deleteTag(ctx iris.Context){
 	type RequestParams struct {
-		TagID uint `json:"tag_id"`
+		TagID string `json:"tag_id"`
 	}
 	type ResponseParams struct {
 		Code    int    `json:"code"`
 		Message string `json:"msg"`
 	}
 
-	//authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
-	//
-	//// parse params
-	//requestParams := RequestParams{}
-	//err := ctx.ReadJSON(&requestParams)
-	//if err != nil {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 406,
-	//		Message: "Params Error",
-	//	})
-	//	return
-	//}
-	//
-	//StickerDatabase.DeleteTag(authResult.AccountID, requestParams.TagID)
-	//
-	//ctx.JSON(ResponseParams{
-	//	Code: 200,
-	//	Message: "Success",
-	//})
+	authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
+
+	// parse params
+	requestParams := RequestParams{}
+	err := ctx.ReadJSON(&requestParams)
+	if err != nil {
+		ctx.JSON(ResponseParams{
+			Code: 406,
+			Message: "Params Error",
+		})
+		return
+	}
+
+	deleteResult := StickerModule.GetOperator().DeleteTag(authResult.AccountID, requestParams.TagID)
+
+	ctx.JSON(ResponseParams{
+		Code: deleteResult.Code,
+		Message: deleteResult.Message,
+	})
 }
 
 func queryTagList(ctx iris.Context){
 	type ResponseParamsItem struct {
-		TagID      uint   `json:"tag_id"`
-		CreateTime int    `json:"create_time"`
-		UpdateTime int    `json:"update_time"`
+		TagID      string   `json:"tag_id"`
+		CreateTime int64    `json:"create_time"`
+		UpdateTime int64    `json:"update_time"`
 		Name       string `json:"name"`
 		Icon       string `json:"icon"`
 		Color      int    `json:"color"`
 		Sort       int    `json:"sort"`
-		Extra      string `json:"extra"`
 	}
 	type ResponseParams struct {
 		Code    int                  `json:"code"`
@@ -109,34 +110,34 @@ func queryTagList(ctx iris.Context){
 		Data    []ResponseParamsItem `json:"data"`
 	}
 
-	//authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
-	//
-	//queryResponse := StickerDatabase.QueryAllTag(authResult.AccountID)
-	//
-	//if queryResponse.Code != 200 {
-	//	ctx.JSON(ResponseParams{
-	//		Code: 500,
-	//		Message: queryResponse.Message,
-	//	})
-	//}
-	//
-	//var dataList []ResponseParamsItem
-	//for _, tmpItem := range queryResponse.Data {
-	//	dataList = append(dataList, ResponseParamsItem{
-	//		TagID: tmpItem.ID,
-	//		CreateTime: tmpItem.CreateTime,
-	//		UpdateTime: tmpItem.UpdateTime,
-	//		Name: tmpItem.Name,
-	//		Icon: tmpItem.Icon,
-	//		Color: tmpItem.Color,
-	//		Sort: tmpItem.Sort,
-	//		Extra: tmpItem.Extra,
-	//	})
-	//}
-	//
-	//ctx.JSON(ResponseParams{
-	//	Code: 200,
-	//	Message: "Success",
-	//	Data: dataList,
-	//})
+	authResult := ApiMiddleware.AuthAccountMiddleWareGetResponse(ctx)
+
+	queryResult := StickerModule.GetOperator().FindAllTag(authResult.AccountID)
+
+	if queryResult.IsSuccess() {
+		ctx.JSON(ResponseParams{
+			Code: queryResult.Code,
+			Message: queryResult.Message,
+		})
+		return
+	}
+
+	var dataList []ResponseParamsItem
+	for _, tmpItem := range queryResult.Tags {
+		dataList = append(dataList, ResponseParamsItem{
+			TagID: tmpItem.ID.Hex(),
+			CreateTime: tmpItem.CreateTime,
+			UpdateTime: tmpItem.UpdateTime,
+			Name: tmpItem.Name,
+			Icon: tmpItem.Icon,
+			Color: tmpItem.Color,
+			Sort: tmpItem.Sort,
+		})
+	}
+
+	ctx.JSON(ResponseParams{
+		Code: 200,
+		Message: "Success",
+		Data: dataList,
+	})
 }
